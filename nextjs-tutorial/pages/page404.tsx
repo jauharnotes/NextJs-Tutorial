5799f052fa2b6626467ae68c40648b027847a754
jauharnotes <jauharnotes@gmail.com>
2021-06-30 12:22:25 +0700

    custom 404 not be found

---
+++ nextjs-tutorial/pages/page404.tsx
@@ -0,0 +1,10 @@
+import '../styles/globals.css';
+
+export default function custom404() {
+    return (
+        <div>
+            <p className='text404'>ooops...</p>
+            <p className='text404'>Halaman yang anda cari tidak ditemukan</p>
+        </div>
+    )
+}
