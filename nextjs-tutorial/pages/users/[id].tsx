f251038f966b34ead430ca894bc6878623e6502b
jauharnotes <jauharnotes@gmail.com>
2021-07-01 17:53:03 +0700

    Data Fetching getStaticProps

--- nextjs-tutorial/pages/users/[id].tsx
+++ nextjs-tutorial/pages/users/[id].tsx
@@ -1,10 +1,13 @@
+import { useRouter } from "next/dist/client/router";
 import Layout from "../../components/layout/layout";
 
 export default function Dashboard() {
+    const router = useRouter();
+    const { id } = router.query;
     return (
         <Layout pageTitle='Dashboard page'>
             <p>
-                Dashboard Page
+                Dashboard Page {id}
             </p>
         </Layout>
     )
--- nextjs-tutorial/pages/users/index.tsx
+++ nextjs-tutorial/pages/users/index.tsx
@@ -1,7 +1,26 @@
-export default function Users(){
-    return(
-        <div>
-            <p>Default Users</p>
-        </div>
+import Layout from "../../components/layout/layout";
+
+export default function Users(props: { dataUser: any; }) {
+    const { dataUser } = props;
+
+    return (
+        <Layout pageTitle='User Page'>
+            {dataUser.map((key: any) => (
+                <>
+                    <p>{key.name}</p>
+                    <p>{key.email}</p>
+                </>
+            ))}
+        </Layout>
     )
 }
+
+export async function getStaticProps() {
+    const res = await fetch('https://jsonplaceholder.typicode.com/users');
+    const dataUser = await res.json();
+    return {
+        props: {
+            dataUser,
+        }
+    }
+};
