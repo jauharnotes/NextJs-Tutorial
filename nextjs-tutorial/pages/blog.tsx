ee9e55eb207835f8cd5dd0c27e394f99421e1b2a
jauharnotes <jauharnotes@gmail.com>
2021-07-02 19:21:49 +0700

    blog post

--- nextjs-tutorial/pages/blog.tsx
+++ nextjs-tutorial/pages/blog.tsx
@@ -2,8 +2,34 @@ import Layout from "../components/layout/layout";
 
 export default function Blog() {
     return (
-        <Layout pageTitle='Blog Page'>
-            <p>Blog Page</p>
+        <Layout pageTitle="Blog Page">
+            <h3>Title Post1</h3>
+            <p>
+                Lorem ipsum dolor sit amet consectetur adipisicing elit. Aspernatur
+                vero, quia consequuntur eligendi non odio dicta quidem, suscipit nulla
+                iusto debitis repudiandae possimus nostrum commodi quos, dolorem
+                consequatur perferendis? Optio voluptates totam voluptatem harum animi
+                blanditiis. Culpa iusto quibusdam perferendis dolores amet cupiditate
+                consectetur voluptatem labore. Ea sed saepe rem!
+            </p>
+            <h3>Title Post2</h3>
+            <p>
+                Lorem ipsum dolor sit amet consectetur adipisicing elit. Aspernatur
+                vero, quia consequuntur eligendi non odio dicta quidem, suscipit nulla
+                iusto debitis repudiandae possimus nostrum commodi quos, dolorem
+                consequatur perferendis? Optio voluptates totam voluptatem harum animi
+                blanditiis. Culpa iusto quibusdam perferendis dolores amet cupiditate
+                consectetur voluptatem labore. Ea sed saepe rem!
+            </p>
+            <h3>Title Post3</h3>
+            <p>
+                Lorem ipsum dolor sit amet consectetur adipisicing elit. Aspernatur
+                vero, quia consequuntur eligendi non odio dicta quidem, suscipit nulla
+                iusto debitis repudiandae possimus nostrum commodi quos, dolorem
+                consequatur perferendis? Optio voluptates totam voluptatem harum animi
+                blanditiis. Culpa iusto quibusdam perferendis dolores amet cupiditate
+                consectetur voluptatem labore. Ea sed saepe rem!
+            </p>
         </Layout>
-    )
+    );
 }
